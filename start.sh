34f3f31aa58108e4cff3834dc9a54e8b099ed94b
Sagar Shahari <sagarshahari26@gmail.com>
2025-08-08 13:55:01 +0530

    Final fix for start.sh script

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
-    #!/usr/bin/env bash
-    # This script runs the uvicorn server using the explicit path to the venv's Python interpreter.
+#!/usr/bin/env bash
+# This script runs the uvicorn server.
 
-    # Exit immediately if a command exits with a non-zero status.
-    set -e
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
-    # Use the Python interpreter from the virtual environment to run uvicorn
-    ./.venv/Scripts/python -m uvicorn main:app --host 0.0.0.0 --port $PORT
+# Use the Python interpreter from the virtual environment to run uvicorn
+./.venv/bin/python -m uvicorn main:app --host 0.0.0.0 --port $PORT
+    
     
